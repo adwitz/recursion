c99f170194a77376ad2c416f69851067af3bcf7b
adwitz <adam.witzel@gmail.com>
2013-09-04 13:41:42 -0400

    finish stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,51 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+// your code goes here
+	var toString = '';
+	
+	switch(typeof obj){
+		case('number'):
+			toString += String(obj);
+			break;
+		case('string'):
+			toString += "\"" + obj + "\"";
+			break;
+		case('boolean'):
+			toString += String(obj);
+			break;
+		case('undefined'):
+			break;
+		case('function'):
+			break;
+		case('object'):
+			if (obj === null){
+				toString += 'null';
+			} else if (Array.isArray(obj)){
+				toString += '[';
+				_.each(obj, function(value, key){
+					toString += stringifyJSON(value);
+					if (key < obj.length-1){
+						toString += ",";
+					};
+				});
+				toString += ']';
+			} else {
+				toString += '{';
+				var counter = 0;
+				_.each(obj, function(value, key){
+					if (value === undefined || typeof value === 'function'){
+						return toString;
+					};
+					toString += stringifyJSON(key) + ':' + stringifyJSON(value);
+					if (counter < Object.keys(obj).length-1){
+						toString += ",";
+						counter++;
+					};
+				});
+				toString += "}";
+			};
+			break;
+		};
+	return toString;
 };
